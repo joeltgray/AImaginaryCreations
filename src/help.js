d75786f66f270171a31a42e67a807c0ba211fb1b
Joel Gray <joeltgray@gmail.com>
2023-03-07 10:01:02 +0000

    Add a help script to display usage

---
+++ src/help.js
@@ -0,0 +1,10 @@
+console.log('Usage \
+    \nYou must provide a .env file in your root working directory and ensure it has the necessary variables, tokens, username, password etc \
+    \nEnsure you have node.js v19.4.0 installed \
+    \nClone this Git repo to your local environment \
+    \n\nRun npm install from the root working directory to install the project using node \
+    \n\nRun npm run start to begin the program using randomly generated words \
+    \n\nIf you wish to specify your own words to give to the prompt rather than have the words be randomly generated you can pass them into the script like so: \
+    \n\nRun npm run start -- helicopter to pass the word helicopter in as your intended word \
+    \nOr: \
+    \nRun npm run start -- \"more words\" to pass in multiple words in one go.')
